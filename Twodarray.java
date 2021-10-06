9d671ec0942c0c3af220ae360beb61559c601dc7
Debadyuti Karmakar <62183757+DEBADYUTI-KARMAKAR@users.noreply.github.com>
2021-10-06 22:18:15 +0530

    2D Array

---
+++ Twodarray.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+
+public class Twodarrayuser{
+	public static void main(String[] args){
+
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	int m = sc.nextInt();	
+	int[][] a = new int[n][m];
+
+	//store input
+	
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			a[i][j] = sc.nextInt();
+		}
+			
+	}
+
+	// how to print
+	
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			System.out.print(a[i][j]+" ");
+		}
+		System.out.println();
+	
+	}
+
+
+
+	}
+
+
+}
